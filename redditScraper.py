a82e3605b7318c78c06dddc4a785a942e6fe7e5b
Hugo <hugorosa123@gmail.com>
2024-08-30 20:09:57 +1200

    Completed nlp before database connection

--- redditScraper.py
+++ redditScraper.py
@@ -12,69 +12,89 @@ reddit = praw.Reddit("bot1")
 #Choosing the subreddits to search for comments in
 subreddits = reddit.subreddit("wellington+auckland+thetron+chch+dunedin+tauranga+newzealand")
 
+#List of acceptable labels for NLP tagging
+labels = {
+    'EVENT', 'FAC', 'GPE', 'LAW', 'LOC', 'NORP', 'ORG', 'PERSON', 'PRODUCT', 'WORK_OF_ART'
+}
+
 #Load comments replied to from file
-if not os.path.isfile("comments_replied_to.txt"):
-    print("Creating comments_replied_to.txt")
-    comments_replied_to = []
+if not os.path.isfile("comments_seen.txt"):
+    print("Creating comments_seen.txt")
+    comments_seen = []
+    submissions_seen = []
 else:
-    with open("comments_replied_to.txt", "r") as f:
-        print("Loading comments_replied_to.txt")
-        comments_replied_to = f.read()
-        comments_replied_to = comments_replied_to.split("\n")
-        comments_replied_to = list(filter(None, comments_replied_to))
+    with open("comments_seen.txt", "r") as f:
+        print("Loading comments_seen.txt")
+        comments_seen = f.read()
+        comments_seen = comments_seen.split("\n")
+        comments_seen = list(filter(None, comments_seen))
+
 
 #Function to save comments replied to
 def exit_handler():
     print("Exiting")
-    with open("comments_replied_to.txt", "w") as f:
-        for comment_id in comments_replied_to:
+    with open("comments_seen.txt", "w") as f:
+        for comment_id in comments_seen:
             f.write(comment_id + "\n")
     print("Saved comments replied to")
+
+
+def perform_nlp(doc):
+    for ent in doc.ents:
+        print(ent.text, ent.label_)
+        if ent.label_ in labels:
+            nlp_labels = nlp(ent.text)
+
+    return nlp_data(doc._.blob.polarity,doc._.blob.subjectivity, nlp_labels)
+
+
 
 #Main loop to search for comments
 try:
     print("Starting Sentiment Analysis Bot")
-    bot_username = reddit.user.me().name
-    print("Logged in as " + bot_username)
-
     #Loading spaCy
-    nlp = spacy.load("en_core_web_sm")
+    nlp = spacy.load("en_core_web_lg")
     nlp.add_pipe('spacytextblob')
+
     print("Initalized spaCy")
 
     for submission in subreddits.hot(limit=3):
-        doc = nlp(submission.title)
-        print(submission.title)
-        submission_title_polarity = doc._.blob.polarity
-        submission_title_subjectivity = doc._.blob.subjectivity
-        doc = nlp(submission.selftext)
-        submission_body_polarity = doc._.blob.polarity
-        submission_body_subjectivity = doc._.blob.subjectivity
+        if submission not in submissions_seen:
+            submission_title_processed =perform_nlp(submission.title)
+            submission_body_processed = perform_nlp(submission.selftext)
+        else:
+            submissions_seen.append(submission.title)
 
         for comment in submission.comments:
-            print("Found new comment")
-            if comment.author.name != bot_username:
-                if comment.id not in comments_replied_to:
-                    doc = nlp(comment.body)
-                else:
-                    print("Already replied to comment")
-                    print(comment.body)
-                    comments_replied_to.append(comment.id)
+            #print("Found new comment")
+            if comment.id not in comments_seen:
+                comment_processed = perform_nlp(comment)
 
-
-
-
+            else:
+                print("Already replied to comment")
+                print(comment.body)
+                comments_seen.append(comment.id)
 
 
 #Catching case where spaCy's model in not installed
 except OSError:
     from spacy.cli import download
-    download("en_core_web_sm")
+
+    download("en_core_web_lg")
     #Exiting cleanly when the program is interrupted by the user
 except KeyboardInterrupt:
     print("Stopped by user")
     exit_handler()
 #Exiting cleanly when the program is interrupted by the user
-except Exception:
+except Exception as e:
     print("Unknown Program Failure")
+    print(e)
     exit_handler()
+
+class nlp_data:
+    def __init__(self, polarity, subjectivity,labels):
+
+        self.polarity = polarity
+        self.subjectivity = subjectivity
+        self.labels = labels
+
